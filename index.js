39b50af6436a46298ab4b085ca563428f271f326
brainka <brainkarau@gmail.com>
2024-05-19 18:27:00 +1000

    fix on click css for mobile

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const shareButton = document.querySelector('button');
 const socialMediaPopUp = document.querySelector('.social-media');
 
+const authorContainerLeft = document.querySelector('.author-container-left');
+const arrowDown = document.querySelector('.arrow-down');
+const iconContainer = document.querySelector('.icon-container');
+const articleBottom = document.querySelector('.article-bottom');
+
 function handleShareButtonClick() {
 	socialMediaPopUp.classList.toggle('hidden');
+	socialMediaPopUp.classList.toggle('move');
+	authorContainerLeft.classList.toggle('hidden-mobile');
+	arrowDown.classList.toggle('visibility');
+	iconContainer.classList.toggle('move');
+	articleBottom.classList.toggle('move');
 }
 
 shareButton.addEventListener('click', handleShareButtonClick);
