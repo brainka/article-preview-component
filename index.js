c4fb211f2c87661c1b94bbdb98331422b79dff07
brainka <brainkarau@gmail.com>
2024-05-19 15:09:46 +1000

    remove console log

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const socialMediaPopUp = document.querySelector('.social-media');
 
 function handleShareButtonClick() {
 	socialMediaPopUp.classList.toggle('hidden');
-
-	console.log(void socialMediaPopUp.classList.toggle('hidden'));
 }
 
 shareButton.addEventListener('click', handleShareButtonClick);
